d2b56eaa4b295d39617a037f437ecb3a0bba0559
Matheus Corbellini <matheussteindorff@gmail.com>
2025-04-12 02:48:13 -0300

    feat(script): support multiple modals with unique behavior, clicking buttom 'x' to close  and ESC key closing

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+const modals = document.querySelectorAll('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelectorAll('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+let currentModal = null;
+
+//function to close modals
+const closeModal = function () {
+  if (currentModal) {
+    currentModal.classList.add('hidden');
+    overlay.classList.add('hidden');
+    currentModal = null;
+  }
+};
+
+// function to open modals
+const openModal = function (modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    currentModal = modal;
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
+};
+
+// opening the different modals when clicking in it
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    const modalId = this.getAttribute('data-modal');
+    openModal(modalId);
+  });
+}
+
+// closing modal when clicks 'X' buttom
+for (let i = 0; i < btnCloseModal.length; i++) {
+  btnCloseModal[i].addEventListener('click', closeModal);
+}
+
+// closing modal when clicking in the overlay/outside the modal
+overlay.addEventListener('click', closeModal);
+
+// closing modal when clicking 'esc' in the keyboard
+document.addEventListener('keydown', function (event) {
+  if (
+    event.key === 'Escape' &&
+    currentModal &&
+    !currentModal.classList.contains('hidden')
+  ) {
+    closeModal();
+  }
+});
